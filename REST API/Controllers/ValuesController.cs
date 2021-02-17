2c5aea92b6af6ad5c7ef602315cca255d2a2b028
Ben Thompson <35780502+bthompson7@users.noreply.github.com>
2021-02-16 23:03:13 -0500

    Add files via upload

---
+++ REST API/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST_API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+
+
+        [HttpGet]
+        public IEnumerable<Values> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Values
+            {
+                Name = "Hello, World this is my new REST API! Written in C#!",
+                CurrentTime = (int)DateTimeOffset.Now.ToUnixTimeSeconds()
+            })
+            .ToArray();
+        }
+
+
+
+    }
+}
