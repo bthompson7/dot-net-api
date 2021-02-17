58b013aaed40931ca0c56bab662b86dacce3aa3e
Ben Thompson <35780502+bthompson7@users.noreply.github.com>
2021-02-16 23:03:43 -0500

    Add files via upload

---
+++ REST API/Values.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace REST_API
+{
+    public class Values
+    {
+
+        public string Name { get; set; }
+
+        public int CurrentTime { get; set; }
+
+
+
+
+    }
+
+
+}
